0e572f593b147eb3d5dd1d8292d29be82f93bd52
Fayazshaik22 <155606242+Fayazshaik22@users.noreply.github.com>
2024-11-07 23:19:14 +0530

    Add files via upload

---
+++ First1.java
@@ -0,0 +1,12 @@
+class First1
+{
+    public static void main(String args[])
+    {
+    int empno=100;
+    String empname="fayaz";
+    float empsal=300.56F;
+    System.out.println("the given employee number is :"+empno);
+    System.out.println("the given employee name is :"+empname);
+    System.out.println("the given employee salary is: "+empsal);
+    }
+}
