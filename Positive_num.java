177391d7f8444d85fd13de3ae4e0df0320430eaf
Fayazshaik22 <155606242+Fayazshaik22@users.noreply.github.com>
2024-11-10 12:20:45 +0530

    DAY 2

---
+++ Positive_num.java
@@ -0,0 +1,26 @@
+public class Positive_num {
+public static void main(String Args[]) {
+   
+      int number =45;
+   
+       
+   
+      if (number > 0) {
+   
+        System.out.println(number + " is positive.");
+   
+      } else if (number < 0) {
+   
+        System.out.println(number + " is negative.");
+   
+      } else {
+   
+        System.out.println(number + " is zero.");
+   
+      }
+   
+    }
+   
+  }
+  
+  
