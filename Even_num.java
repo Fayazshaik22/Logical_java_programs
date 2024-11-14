779f2d199d892ac30cec3f2f89411e1f5bb324c6
Fayazshaik22 <155606242+Fayazshaik22@users.noreply.github.com>
2024-11-14 19:09:54 +0530

    DAY 3

---
+++ Even_num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Even_Numbers
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Number of is : ");
+		int l =input.nextInt();
+		for(int s=1;s<=l;s++)
+		{
+			if(s%2==0)
+				System.out.println(s);
+		}
+	}
+}
+
