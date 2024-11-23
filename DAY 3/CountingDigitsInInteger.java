705f7b6b98699ad86e9270c13db56a3693d7bb44
Fayazshaik22 <155606242+Fayazshaik22@users.noreply.github.com>
2024-11-23 21:54:26 +0530

    Add files via upload

---
+++ DAY 3/CountingDigitsInInteger.java
@@ -0,0 +1,12 @@
+public class CountingDigitsInInteger {
+    public static void main(String args[]) {
+int count = 0;
+    int num = 1254566;
+       System.out.println("given number:: " + num);
+        while(num != 0){
+          num = num / 10;
+          count++;
+       }
+       System.out.println("Number of digits in the given integer are:: " + count);
+    }
+ }
---
+++ DAY 3/Sum_natural.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sum_natural{
+   public static void main(String[] args) {
+       Scanner S=new Scanner(System.in);
+       System.out.println("Enter the number of limit");
+       int l= S.nextInt();
+int sum=0;
+for(int n=1;n<=l;n++)
+sum= sum+n;
+System.out.println("sum of the numbers:"+Sum);
+
+   }
+}
+
